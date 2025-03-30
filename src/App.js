f05c96864754bdbc1f2ceb4c844c0a2314950b11
Wappyking <Wappyfresh@gmail.com>
2025-03-30 16:12:25 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Home from "./screens/Home";
+// import "tailwindcss/tailwind.css";
+
+function App() {
+  return <Home />;
+}
+
+export default App;
---
+++ src/screens/Home.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="logo192.png" className="App-logo" alt="logo" />
+        <p class=" text-green-900 mt-7 ">coming soon!!!</p>
+        <a
+          className="bg-green-800 mt-5 px-3 py-1 rounded-2xl text-bol"
+          href="https://www.wapstechhive.com/green-luck-app"
+          //   target="_blank"
+          //   rel="noopener noreferrer"
+        >
+          Download Mobile App
+        </a>
+      </header>
+    </div>
+  );
+};
+
+export default Home;
